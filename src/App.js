2c3ffab530a520e4e6db1a1546a45955ead26917
Samir Patil <samirspatil742099@gmail.com>
2023-01-05 11:52:31 +0530

    Inti

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([{id: 1, name:"Todo 1", complete: true}, {id: 2, name:"Todo 2", complete:false}])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <TodoList todos={todos}/>
+      <input type="text"/>
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left todos</div>
+    </>
   );
 }
 
